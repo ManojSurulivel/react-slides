f4d071f56b67cf8928d8252bbbb2793ccd8597d5
manoj S <97394284+ManojSurulivel@users.noreply.github.com>
2024-10-06 12:57:36 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Toggle from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <Toggle slides={[{title:"title1",text:"text1"},{title:"title2",text:"text2"},{title:"title3",text:"text3"},{title:"title4",text:"text4"}]}/>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

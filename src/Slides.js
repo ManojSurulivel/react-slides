1d6edd6f3557b4b8f6f2a7fae1b7cb9702432782
manoj S <97394284+ManojSurulivel@users.noreply.github.com>
2024-10-06 12:56:42 +0700

    Create Slides.js

---
+++ src/Slides.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function Slides({ slides }) {
+  const [position, setPosition] = useState(0);
+
+  const isNextDisabled = position === slides.length - 1;
+  const isPrevDisabled = position === 0;
+
+  return (
+    <div className="slider">
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          disabled={isPrevDisabled}
+          onClick={() => {
+            setPosition(0);
+          }}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={() => {
+            setPosition(position - 1);
+          }}
+          disabled={isPrevDisabled}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={() => {
+            setPosition(position + 1);
+          }}
+          disabled={isNextDisabled}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slides[position].title}</h1>
+        <p data-testid="text">{slides[position].text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Slides;
